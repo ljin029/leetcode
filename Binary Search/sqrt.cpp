b69745dfbefa911540c25d394bb72cfd36e59a77
ljin029 <lei_jin@hotmail.com>
2017-09-08 13:47:35 -0400

    Added reference into sqrt.cpp

--- Binary Search/sqrt.cpp
+++ Binary Search/sqrt.cpp
@@ -12,7 +12,7 @@
 
 /*
  *	The solution:
- *	Binary search approach.
+ *	Binary search approach: For non-negative n, its sqrt <= x/2+1, search in the range (0, x/2+1]
  *	1) Time:  O(log n)
  *	2) Space: O(1)
  *
@@ -21,20 +21,17 @@
  class Solution {
  public:
 	 int mySqrt(int x) {
-		 if (!x) return 0;
+		 long long i=0, j=x/2+1;
 		 
-		 int low = 1, high = x;
-		 int res;
-		 while (low<=high){
-			 int mid = (low+high)/2;
-			 if (mid <= x/mid) {
-				 low = mid + 1;
-				 res = mid;
-			 }
-			 else high = mid - 1;
+		 while (i<=j)
+		 {
+			 long long mid = (i+j)/2;
+			 long long sqrt=mid * mid;
+			 if (sqrt==x) return mid;
+			 else if (sqrt<x) i=mid+1;
+			 else j=mid-1;
 		 }
-		 
-		 return res;
+		 return j;
 	 }
  };
 
