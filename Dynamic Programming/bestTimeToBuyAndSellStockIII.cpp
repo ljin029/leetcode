0886e59f340f7d2b580fa1064ec16ad0f40a3726
ljin029 <lei_jin@hotmail.com>
2017-11-20 15:39:35 -0500

    Best Time to Buy and Sell Stock III

---
+++ Dynamic Programming/bestTimeToBuyAndSellStockIII.cpp
@@ -0,0 +1,44 @@
+// Source : https://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+// Author : ljin029
+// Date   : 2017-11-20
+
+/********************************************************************************** 
+* 
+* Say you have an array for which the ith element is the price of a given stock on day i.
+* 
+* Design an algorithm to find the maximum profit. You may complete at most two transactions.
+* 
+* Note:
+* You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+*               
+**********************************************************************************/
+
+/*
+** dp[i][j] represents the max profit up until prices[j] and i is the number of transaction
+** dp[i][j] = max (dp[i][j-1], prices[j]-prices[m]+dp[i][m]) for m=[0...j-1];
+** Hence, tmpMax = max(tmpMax, dp[i][m]-prices[m]) for m=[0...j-1];
+** dp[i][j] = max (dp[i][j-1], prices[j]+tmpMax);
+** maxProf=max(maxProf, dp[i][j]);
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size(), kk = 2;
+        if (n<=1) return 0;
+        vector<vector<int>> dp(kk+1,vector<int>(n, 0));
+        
+        int maxProf=0;
+        for(int i=1; i<=kk; ++i){
+            int tmpMax = dp[i][0]-prices[0];
+            for(int j=1; j<n; ++j){
+                dp[i][j]=max(dp[i][j-1], prices[j]+tmpMax);
+                tmpMax=max(tmpMax, dp[i-1][j]-prices[j]);
+                maxProf=max(maxProf, dp[i][j]);
+            }
+        }
+        return maxProf;
+    }
+};
+
+
